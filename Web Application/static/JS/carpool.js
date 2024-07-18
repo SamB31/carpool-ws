63e8c17432591e9a12e010fb289b8da0371227fe
samb31 <asblanton31@gmail.com>
2024-07-18 12:55:18 -0500

    individual checkout

--- Web Application/static/JS/carpool.js
+++ Web Application/static/JS/carpool.js
@@ -1,5 +1,5 @@
-const socket = new WebSocket('ws://localhost:8080');
-//const socket = new WebSocket('ws://192.168.1.136:8080');
+//const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('ws://192.168.1.136:8080');
 
 
 socket.onopen = () => {
