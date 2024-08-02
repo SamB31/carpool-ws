6d6d4ff27bb4945828b9d2a117aa5d00e2793a8f
samb31 <asblanton31@gmail.com>
2024-08-02 13:30:02 -0500

    redis

--- Web Application/src/server.js
+++ Web Application/src/server.js
@@ -22,7 +22,7 @@ const PORT = 80; // Make sure the port is correct
 const HOST = '0.0.0.0'; 
 app.listen(PORT, HOST, async () => {
     try {
-        await testConnection();
+        await testDatabaseConnection();
         console.log(`Server is running on http://${HOST}:${PORT}`);
     } catch (error) {
         console.error('Unable to connect to the database:', error);
