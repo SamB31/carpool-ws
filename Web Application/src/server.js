2e1df96d580e9814de76d2ea763f7d820b19b9fa
samb31 <asblanton31@gmail.com>
2024-08-02 13:37:00 -0500

    redis

--- Web Application/src/server.js
+++ Web Application/src/server.js
@@ -8,21 +8,11 @@ const server = http.createServer(app);
 // Initialize WebSocketHandler with the HTTP server
 WebSocketHandler.init(server);
 
-// Test the database connection
-async function testDatabaseConnection() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection to the database has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
 
 const PORT = 80; // Make sure the port is correct
 const HOST = '0.0.0.0'; 
 app.listen(PORT, HOST, async () => {
     try {
-        await testDatabaseConnection();
         console.log(`Server is running on http://${HOST}:${PORT}`);
     } catch (error) {
         console.error('Unable to connect to the database:', error);
