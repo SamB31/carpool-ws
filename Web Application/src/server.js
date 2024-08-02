0dd048236e657b3d1b616a1ce6ade57aa2887880
samb31 <asblanton31@gmail.com>
2024-08-02 00:10:22 -0500

    redis

--- Web Application/src/server.js
+++ Web Application/src/server.js
@@ -11,5 +11,11 @@ WebSocketHandler.init(server);
 // Start the server
 const PORT = 8080;
 server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+    try {
+        sequelize.authenticate();
+        console.log('Database connection established.');
+        console.log(`Server is running on http://localhost:${PORT}`);
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 });
