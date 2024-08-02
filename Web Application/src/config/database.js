9ad574fd3c07acfc347573b1c0ee289f056cf1c7
samb31 <asblanton31@gmail.com>
2024-08-02 00:20:22 -0500

    redis

--- Web Application/src/config/database.js
+++ Web Application/src/config/database.js
@@ -6,25 +6,11 @@ const sequelize = new Sequelize('carpool', 'samb31', 'Asb2107!', {
     port: 5432,
     logging: false,
     pool: {
-        max: 5,
+        max: 100,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
 });
-
-// Function to test the database connection
-async function testConnection() {
-    try {
-        await sequelize.authenticate(); // Authenticate the connection
-        console.log('Database connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-        // Handle or rethrow error as needed
-    }
-}
-
-// Call testConnection at startup to ensure connectivity
-testConnection();
 
 module.exports = { sequelize };
